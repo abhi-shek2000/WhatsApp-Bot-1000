e038d5ab3f2afde686f666ece56e1fd071584a12
technical_geek <52378806+abhi-shek2000@users.noreply.github.com>
2020-07-28 16:57:13 +0530

    Update WhatsappBotTosend1000msgs3.py

--- Version1/WhatsappBotTosend1000msgs3.py
+++ Version1/WhatsappBotTosend1000msgs3.py
@@ -1,17 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-driver = webdriver.Chrome()
-driver.get("https://web.whatsapp.com/")
-time.sleep(20)
+
+PATH = "C:\Program Files (x86)/chromedriver.exe"   #Path of Chromedriver (Your Path may be different)
+bot = webdriver.Chrome(PATH)
+
+Name = "CodeinVeins"  #Enter Name of the person whom you want to send message
+
+bot.get('https://web.whatsapp.com/')
+
+time.sleep(10)  #wait for 10 secs
+
+person = bot.find_element_by_xpath("//span[@title = '{}']".format(Name))
+
+person.click()
+
+msg_box = bot.find_element_by_class_name('_3uMse')
 
 
-user_name = input()
-msg = input()
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(user_name))
-user.click()
-msg_box = driver.find_element_by_class_name('_13mgZ')
+# add for loop
+
 for i in range(1000):
-    msg_box.send_keys(msg)
+    msg_box.send_keys("This message is sent using Python ")  # Type your own message
+    time.sleep(1.5)
     msg_box.send_keys(Keys.RETURN)
+    time.sleep(1)
+
 
